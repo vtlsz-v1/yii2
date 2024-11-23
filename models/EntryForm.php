54ba97c15acf56e7d7f8fd7584a72f2f96fb12c0
vtlsz-v1 <vtlsz.v1@gmail.com>
2024-11-24 02:16:44 +0300

    формы2

--- models/EntryForm.php
+++ models/EntryForm.php
@@ -10,4 +10,24 @@ class EntryForm extends Model
     public $name;
     public $email;
     public $text;
+
+    // правила валидации формы
+    public function rules()
+    {
+        return [
+            [['name', 'email', 'text'], 'required'], // обязательные к заполнению поля
+            ['email', 'email'], // поле предназначено для ввода email
+        ];
+    }
+
+    public function attributeLabels()
+    {
+        // переименовываем поля формы
+        return [
+            'name' => 'Имя:',
+            'email' => 'E-mail:',
+            'text' => 'Текст сообщения:'
+        ];
+    }
+    // аналогичный метод можно применить к hint
 }
--- views/test/index.php
+++ views/test/index.php
@@ -5,18 +5,42 @@ use yii\helpers\Html; // требуется для создания кнопки
 <div class = "col-md-12">
     <h2>Страница с формой</h2>
 
-    <?php $form = ActiveForm::begin() // открытие формы ?>
+    <?php $form = ActiveForm::begin([ // открытие формы
+            'id' => 'my-form', // переопределяем id формы
+            //'enableClientValidation' => false, // отключение клиентской валидации
+            'options' => [ // массив дополнительных настроек
+                'class' => 'form-horizontal', // определяем класс bootstrap
+            ],
+            'fieldConfig' => [ // конфигурации формы
+                    // шаблон для поля формы
+                'template' => "{label} \n <div class='col-md-5'> {input} </div> \n <div class='col-md-5'> {hint} </div> \n 
+                            <div class='col-md-5'> {error} </div>",
+                'labelOptions' => ['class' => 'col-md-2 control-label'], // опции для label
+            ]
+    ]) ?>
 
     <!--метод field() используется для создания полей формы
         1-й параметр - модель формы
         2-й параметр - поле формы-->
-    <?= $form->field($model, 'name') ?>
-    <?= $form->field($model, 'email') ?>
-    <?= $form->field($model, 'text')->textarea(['rows' => 7]) // текстовая область из 7 строк ?>
+    <?= $form->field($model, 'name')->textInput(['placeholder' => 'Введите имя']) ?>
+
+    <?= $form->field($model, 'email')->hint('<span class="text-info">Укажите здесь Ваш email</span>')
+        ->input('email', ['placeholder' => 'Введите Ваш email']) // подсказка внутри поля, email - тип поля ?>
+
+    <?= $form->field($model, 'text', [
+        // шаблон для поля формы
+        'template' => "<!--{label} \n --> <div class='col-md-5'> {input} </div> \n <div class='col-md-5'> {hint} </div> \n 
+                                          <div class='col-md-7'> {error} </div>", /*здесь название поля не отобразится*/
+        'labelOptions' => ['class' => 'col-md-2 control-label'], // опции для label
+    ])->textarea(['rows' => 7, 'placeholder' => 'Введите текст'])
+    // текстовая область из 7 строк ?>
     <!--   ['rows' => 10] - массив опций метода   -->
 
+    <!--кнопка-->
     <div class="form-group">
-        <?= HTML::submitButton('Отправить', ['class' => 'btn btn-outline-dark my-3']) // кнопка ?>
+        <div class="d-grid gap-2 col-md-5 col-md-offset-2">
+            <?= HTML::submitButton('Отправить', ['class' => 'btn btn-outline-dark my-3', 'type' => 'button']) ?>
+        </div>
     </div>
 
     <?php ActiveForm::end() // закрытие формы ?>
