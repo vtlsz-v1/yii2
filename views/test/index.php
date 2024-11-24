8a304ac594dff8f40e0ba614259bb39708d871f9
vtlsz-v1 <vtlsz.v1@gmail.com>
2024-11-25 00:31:07 +0300

    формы2

--- views/test/index.php
+++ views/test/index.php
@@ -1,22 +1,31 @@
 <?php
-use yii\widgets\ActiveForm; // подключаем виджет для создания формы
+//use yii\widgets\ActiveForm; // подключаем виджет для создания формы
+use yii\bootstrap5\ActiveForm; // подключаем виджет для создания формы
 use yii\helpers\Html; // требуется для создания кнопки
 ?>
 <div class = "col-md-12">
-    <h2>Страница с формой</h2>
+    <h2 class="my-3">Страница с формой</h2>
 
     <?php $form = ActiveForm::begin([ // открытие формы
+            'layout' => 'horizontal', // горизонтальная форма
+            'options' => ['class' => 'signup-form form-register1'],
             'id' => 'my-form', // переопределяем id формы
             //'enableClientValidation' => false, // отключение клиентской валидации
-            'options' => [ // массив дополнительных настроек
+            /*'options' => [ // массив дополнительных настроек
                 'class' => 'form-horizontal', // определяем класс bootstrap
-            ],
+            ],*/
+
             'fieldConfig' => [ // конфигурации формы
                     // шаблон для поля формы
                 'template' => "{label} \n <div class='col-md-5'> {input} </div> \n <div class='col-md-5'> {hint} </div> \n 
                             <div class='col-md-5'> {error} </div>",
                 'labelOptions' => ['class' => 'col-md-2 control-label'], // опции для label
             ]
+
+        /*'fieldConfig' =>
+            [ 'template' => "{label}\n{beginWrapper}\n{input}\n{hint}\n{error}\n{endWrapper}",
+                'horizontalCssClasses' => [ 'label' => 'col-sm-2', 'offset' => 'col-sm-offset-4',
+                'wrapper' => 'col-sm-5', 'error' => '', 'hint' => '', ], ],*/
     ]) ?>
 
     <!--метод field() используется для создания полей формы
@@ -29,16 +38,16 @@ use yii\helpers\Html; // требуется для создания кнопки
 
     <?= $form->field($model, 'text', [
         // шаблон для поля формы
-        'template' => "<!--{label} \n --> <div class='col-md-5'> {input} </div> \n <div class='col-md-5'> {hint} </div> \n 
-                                          <div class='col-md-7'> {error} </div>", /*здесь название поля не отобразится*/
-        'labelOptions' => ['class' => 'col-md-2 control-label'], // опции для label
+        //'template' => "<!--{label} \n --> <div class='col-md-5'> {input} </div> \n <div class='col-md-5'> {hint} </div> \n
+        //<div class='col-md-2'> {error} </div>", /*здесь название поля не отобразится*/
+        //'labelOptions' => ['class' => 'col-md-2 control-label'], // опции для label
     ])->textarea(['rows' => 7, 'placeholder' => 'Введите текст'])
     // текстовая область из 7 строк ?>
     <!--   ['rows' => 10] - массив опций метода   -->
 
     <!--кнопка-->
     <div class="form-group">
-        <div class="d-grid gap-2 col-md-5 col-md-offset-2">
+        <div class="d-grid gap-2 col-md-9 col-md-offset-2">
             <?= HTML::submitButton('Отправить', ['class' => 'btn btn-outline-dark my-3', 'type' => 'button']) ?>
         </div>
     </div>
@@ -46,3 +55,4 @@ use yii\helpers\Html; // требуется для создания кнопки
     <?php ActiveForm::end() // закрытие формы ?>
 
 </div>
+
