cf1da4862df4f4db531b24ca39af77577f1cbab6
vtlsz-v1 <vtlsz.v1@gmail.com>
2024-11-28 03:32:38 +0300

    модели

---
+++ country.sql
@@ -0,0 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1
+-- Время создания: Ноя 28 2024 г., 01:24
+-- Версия сервера: 10.4.24-MariaDB
+-- Версия PHP: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `yii2basic`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `country`
+--
+
+CREATE TABLE `country` (
+  `code` char(2) NOT NULL,
+  `name` char(52) NOT NULL,
+  `population` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `country`
+--
+
+INSERT INTO `country` (`code`, `name`, `population`) VALUES
+('AU', 'Australia', 24016400),
+('BR', 'Brazil', 205722000),
+('CA', 'Canada', 35985751),
+('CN', 'China', 1375210000),
+('DE', 'Germany', 81459000),
+('FR', 'France', 64513242),
+('GB', 'United Kingdom', 65097000),
+('IN', 'India', 1285400000),
+('RU', 'Russia', 146519759),
+('US', 'United States', 322976000);
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `country`
+--
+ALTER TABLE `country`
+  ADD PRIMARY KEY (`code`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
