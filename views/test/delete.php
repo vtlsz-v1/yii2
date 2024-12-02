58f1bf991efddce55ad208b6d3bc1032f2212d9c
vtlsz-v1 <vtlsz.v1@gmail.com>
2024-12-03 02:40:39 +0300

    ActiveRecord3

--- views/test/delete.php
+++ views/test/delete.php
@@ -11,7 +11,7 @@
     <?php if(\Yii::$app->session->hasFlash('error')) : // если в сессию записано сообщение с ключом error ?>
         <div class="alert alert-danger alert-dismissible" role="alert">
             <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <?= Yii::$app->session->getFlash('success') // выводим сообщение на экран ?>
+            <?= Yii::$app->session->getFlash('error') // выводим сообщение на экран ?>
         </div>
     <?php endif; ?>
 
--- views/test/update.php
+++ views/test/update.php
@@ -17,7 +17,7 @@ use yii\bootstrap5\ActiveForm;
     <?php if(\Yii::$app->session->hasFlash('error')) : // если в сессию записано сообщение с ключом error ?>
         <div class="alert alert-danger alert-dismissible" role="alert">
             <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <?= Yii::$app->session->getFlash('success') // выводим сообщение на экран ?>
+            <?= Yii::$app->session->getFlash('error') // выводим сообщение на экран ?>
         </div>
     <?php endif; ?>
 
